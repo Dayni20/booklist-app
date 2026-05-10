9d80a0904636cba8bd65e042af89c1f43d69d418
Dayni20 <dayniliss@hotmail.com>
2026-05-10 11:18:50 -0500

    feat: add Book and NewBook TypeScript interfaces

---
+++ src/types/book.ts
@@ -0,0 +1,14 @@
+export interface Book {
+  id: number;
+  title: string;
+  author: string;
+  year: number;
+  genre: string;
+}
+
+export interface NewBook {
+  title: string;
+  author: string;
+  year: string;
+  genre: string;
+}
